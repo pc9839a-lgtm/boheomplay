3a89b9f31f953524abea1a147de2bdec3da747b0
pc9839a-lgtm <pc9839a@naver.com>
2026-07-09 16:38:47 +0900

    Add temporary admin password config

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,5 @@
 window.APP_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbxCtjwoWhUyi1HxD93HfS_09umPz_kNDj0fp-necgFH37LU5JbkMnS3IZFxSDAZbeVWzw/exec',
-  submitTimeout: 30000
+  submitTimeout: 30000,
+  adminPassword: 'boheomplay2026'
 };
