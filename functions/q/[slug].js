ecaabebea684e89dbbb415d5a418036257aafe55
pc9839a-lgtm <pc9839a@naver.com>
2026-07-15 08:22:48 +0900

    Route 2026-07-15 insurance questions

--- functions/q/[slug].js
+++ functions/q/[slug].js
@@ -2,9 +2,13 @@ import { questions, renderQuestionPage, renderNotFound, htmlResponse } from '../
 import { extraQuestions, renderExtraQuestionPage } from '../_extra-qa.js';
 import { dailyQuestions20260713, renderDailyQuestionPage20260713 } from '../_qa-2026-07-13.js';
 import { dailyQuestions20260714, renderDailyQuestionPage20260714 } from '../_qa-2026-07-14.js';
+import { dailyQuestions20260715, renderDailyQuestionPage20260715 } from '../_qa-2026-07-15.js';
 
 export async function onRequest(context) {
   const slug = String(context.params.slug || '').trim();
+
+  const dailyQuestion20260715 = dailyQuestions20260715.find((item) => item.slug === slug);
+  if (dailyQuestion20260715) return htmlResponse(renderDailyQuestionPage20260715(dailyQuestion20260715));
 
   const dailyQuestion20260714 = dailyQuestions20260714.find((item) => item.slug === slug);
   if (dailyQuestion20260714) return htmlResponse(renderDailyQuestionPage20260714(dailyQuestion20260714));
