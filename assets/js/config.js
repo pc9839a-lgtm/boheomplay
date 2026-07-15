087e089a9bb330dcbb0396c1a264ab875673387c
pc9839a-lgtm <pc9839a@naver.com>
2026-07-15 10:49:50 +0900

    Load board router and all-consent controls directly

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,3 +2,16 @@ window.APP_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbxCtjwoWhUyi1HxD93HfS_09umPz_kNDj0fp-necgFH37LU5JbkMnS3IZFxSDAZbeVWzw/exec',
   submitTimeout: 30000
 };
+
+(function loadQuestionEnhancements(){
+  [
+    '/assets/js/consent-all.js?v=20260715-v1',
+    '/assets/js/board-router.js?v=20260715-v1'
+  ].forEach((src)=>{
+    if(document.querySelector(`script[src^="${src.split('?')[0]}"]`)) return;
+    const script=document.createElement('script');
+    script.src=src;
+    script.async=false;
+    document.head.appendChild(script);
+  });
+})();
