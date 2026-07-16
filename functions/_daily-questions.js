1bef1051a7a4eca8c79ecb31c39cd6ec4b90d2b5
pc9839a-lgtm <pc9839a@naver.com>
2026-07-16 09:02:41 +0900

    Register 2026-07-16 daily questions

--- functions/_daily-questions.js
+++ functions/_daily-questions.js
@@ -1,10 +1,12 @@
 import { dailyQuestions20260713 } from './_qa-2026-07-13.js';
 import { dailyQuestions20260714 } from './_qa-2026-07-14.js';
 import { dailyQuestions20260715 } from './_qa-2026-07-15.js';
+import { dailyQuestions20260716 } from './_qa-2026-07-16.js';
 
 const withDate = (items, updatedAt) => items.map((item) => ({ ...item, updatedAt }));
 
 export const dailyQuestions = [
+  ...withDate(dailyQuestions20260716, '2026-07-16'),
   ...withDate(dailyQuestions20260715, '2026-07-15'),
   ...withDate(dailyQuestions20260714, '2026-07-14'),
   ...withDate(dailyQuestions20260713, '2026-07-13')
